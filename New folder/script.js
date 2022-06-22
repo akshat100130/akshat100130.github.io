3dd3b10c956e2002f3da484f7106393d2688f5aa
akshat100130 <54878217+akshat100130@users.noreply.github.com>
2022-06-22 19:33:06 +0530

    Udbhav

---
+++ New folder/script.js
@@ -0,0 +1,26 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCEX6s3DHezuj7lkeGRML8omy2zcaxOBiw",
+//   authDomain: "udbhavsel-37575.firebaseapp.com",
+//   projectId: "udbhavsel-37575",
+//   storageBucket: "udbhavsel-37575.appspot.com",
+//   messagingSenderId: "787669366631",
+//   appId: "1:787669366631:web:0c80c67ae01049b61ee1c2"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
+function formSubmit(){
+  document.getElementById('formSubmit').style.display='none';
+  document.getElementById('formConfirm').style.display='block';
+  console.log(subnmitButton);
+
+}
+
